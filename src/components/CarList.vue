f3589fb2c9b1e2c4749e609829025c1eb1f8790f
Куаныш Амиржан Куанышович <@itstep.loc>
2023-09-08 20:44:52 +0600

    awf

---
+++ src/components/CarList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import CarItem from '@/components/CarItem.vue'
+import { defineProps } from 'vue'
+
+defineProps({
+  cars: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <section class="cars" v-for="car in cars" :key="car">
+    <CarItem :car="car" />
+  </section>
+</template>
+
+<style scoped></style>
